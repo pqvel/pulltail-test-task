65cc507106ba0a47bd8824db08a78afc28807871
Pavel Salianik <solyqnik@gmail.com>
2024-07-25 10:50:47 +0300

    +

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,7 @@ import { onMounted } from "vue";
 import { Container, Section } from "./components/ui/wrappers";
 import Filters from "./components/Filters.vue";
 import Loads from "./components/loads/Loads.vue";
-import { useStore } from "./core/store/store";
 import { useLoadsService } from "./core/services/loadsService";
-
-const store = useStore();
 
 onMounted(() => {
   const loadsService = useLoadsService();
